4de5f89ea8a5b6af0e3b9a1c39879663ae251752
Raghav-0007 <rmyadav514@gmail.com>
2025-03-06 16:54:13 +0530

    [Radhamohan yadav] . Add Added Event Binding to launch a website on clicking the logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Helloworld';
   imagePath = 'assets/Logo.png';
+  url='https://www.bridgelabz.com';
 
   ngOnInit(): void{
     this.title="Hello from bridgelabz."
+  }
+
+  onClick(){
+    alert("Image clicked!");
+    console.log("save button is clicked");
+    window.open(this.url, "_blank");
   }
 }
