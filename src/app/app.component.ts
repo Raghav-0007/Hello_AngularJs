1c4213d81a8beea2516f16e657bad3c09d72fe0a
Raghav-0007 <rmyadav514@gmail.com>
2025-03-07 00:44:52 +0530

    [Radhamohan yadav] . Add input validation for userName with error handling in App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -5,7 +6,7 @@ import { log } from 'node:console';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -15,6 +16,8 @@ export class AppComponent {
   imagePath = 'assets/Logo.png';
   url='https://www.bridgelabz.com';
   userName: String ="";
+  errorMessage: string = '';
+
 
   ngOnInit(): void{
     this.title="Hello from bridgelabz."
@@ -24,5 +27,18 @@ export class AppComponent {
     alert("Image clicked!");
     console.log("save button is clicked");
     window.open(this.url, "_blank");
+  }
+
+  validateUserName() {
+    console.log("User entered:", this.userName);  // Check if input is detected
+    const namePattern = /^[A-Z][a-zA-Z]{2,}$/;  
+
+    if (!this.userName.match(namePattern)) {
+      this.errorMessage = 'Name must start with a capital letter and have at least 3 letters.';
+      console.log("Validation failed:", this.errorMessage);
+    } else {
+      this.errorMessage = '';
+      console.log("Validation passed! Name:", this.userName);
+    }
   }
 }
